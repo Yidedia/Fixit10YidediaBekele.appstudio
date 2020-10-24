8e51805564b2e3f209edf6889c7e392b455e2d86
Yidedia <70745425+Yidedia@users.noreply.github.com>
2020-10-24 12:08:30 -0500

    Complete Change
    Had to delete both forms and start from scratch

--- forms/customerSelect/customerSelect.js
+++ forms/customerSelect/customerSelect.js
@@ -5,61 +5,62 @@ let results = ""
 let pw = "Berhanu#0721" // put your database password here
 let userName = 'ymb85951'
 
-customerSelect.onshow = function() {
-  txtCustomer.style.height = "100px"
-}
 
-customerSelect.onshow = function() {
-  drpCustFind.clear()
+CustomerSelect.onshow=function(){
+  drpSelect.clear()
   query = "SELECT name from customer"
   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ymb85951&pass=" + pw + "&database=ymb85951&query=" + query)
 
   if (req.status == 200) { //transit worked.
-    customerResultsFind = JSON.parse(req.responseText)
-    console.log(customerResultsFind)
+    customerSelectR = JSON.parse(req.responseText)
+    console.log(customerSelectR)
   }
-  if (customerResultsFind.length == 0) {
+  if (customerSelectR.length == 0) {
     // if no customers in a table brings back this message
     NSB.MsgBox("There are no customers to delete.")
   } else {
     //a loop that adds all the customers in the array to the dropdown.
-    for (i = 0; i <= customerResultsFind.length - 1; i++)
-      drpCustFind.addItem(customerResultsFind[i])
+    for (i = 0; i <= customerSelectR.length - 1; i++)
+      drpSelect.addItem(customerSelectR[i])
   }
 }
 
-drpCustFind.onclick = function(s) {
+
+drpSelect.onclick=function(s){
   // this 'if' kicks user out if they  just clicked on control 
   // but not on one item in the list.
   if (typeof(s) == "object")
     return
   else { // the user picked something
-    drpCustFind.value = s // make dropdown show the choice the user made
+    drpSelect.value = s // make dropdown show the choice the user made
     query = `SELECT state from customer WHERE name = '${s}'`
     //Grab the state of the customer chosen
     req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ymb85951&pass=" + pw + "&database=ymb85951&query=" + query)
 
     if (req.status == 200) { //transit worked.
       //save the sate of the customer 
-      customerState = JSON.parse(req.responseText)
-      console.log(customerState)
+      customerSelectState = JSON.parse(req.responseText)
+      console.log(customerSelectState)
     }
-    query = `SELECT name from customer WHERE state = '${customerState[0]}'`
+    query = `SELECT name from customer WHERE state = '${customerSelectState[0]}'`
     // get the other customers who have the same state
     req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ymb85951&pass=" + pw + "&database=ymb85951&query=" + query)
-
+    
     if (req.status == 200) { //transit worked.
       //save the sate of the customer 
-      customerWithTheSameState = JSON.parse(req.responseText)
-      console.log(customerWithTheSameState)
+      customerSelectSameState = JSON.parse(req.responseText)
+      console.log(customerSelectSameState)
     }
-
+    
     let customerMessage = ""
-    for (i = 0; i <= customerWithTheSameState.length - 1; i++)
-      customerMessage = customerMessage + customerWithTheSameState[i] + "\n"
-    txtCustomer.value = customerMessage
-  }
+    for (i = 0; i <= customerSelectSameState.length - 1; i++)
+      customerMessage = customerMessage + customerSelectSameState[i] + "\n"
+    // clear txt and then change
+    txtCustomerSelect.value = customerMessage
+  }  
 }
-nxt.onclick=function(){
+/*
+btnDelete.onclick=function(){
   ChangeForm(customerDelete)
 }
+*/
